400c62d6336565f2a26eb71f72b1e6d6fa54aea9
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-11-22 13:13:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-// complete the given function
+function palindrome(str) {
+    const cleanString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
-function palindrome(str){
+    // Compare characters from both ends towards the center
+    for (let i = 0, j = cleanString.length - 1; i < j; i++, j--) {
+        if (cleanString[i] !== cleanString[j]) {
+            return false; // Characters do not match
+        }
+    }
 
+    return true; // All characters matched
 }
-module.exports = palindrome
+
+module.exports = palindrome;
+
